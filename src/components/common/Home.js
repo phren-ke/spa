ed4782a41930f04d33a6c680a47b744cf75ea4fe
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2023-08-27 01:25:39 +0300

    #3: - add hero section

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,13 +1,62 @@
-import { Card, CardContent } from '@mui/material';
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
+import spa_one from "../assets/spa-1.jpg";
+import { makeStyles } from '@mui/styles';
+import { Button, Grid, Typography, Box } from '@mui/material';
+
+const useStyles = makeStyles({
+    heroSection: {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${spa_one})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 6
+    },
+    heading: {
+        color: 'white',
+        fontSize: '3.5rem !important',
+        marginBottom: 2,
+        '@media (min-width:600px)': {
+            fontSize: '5rem'
+        },
+        whiteSpace: 'normal',
+        overflowWrap: 'break-word'
+    },
+    button: {
+        marginBottom: 2,
+        color: 'white !important',
+        borderColor: 'white',
+        '&:hover': {
+            backgroundColor: '#1976d2 !important'
+        }
+    },
+    buttonContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        width: '100%'
+    }
+});
 
 export const Home = () => {
+    const classes = useStyles();
     return (
-        <Link to="/Login">
-            <Card>
-                <CardContent>Home</CardContent>
-            </Card>
-        </Link>
+        <Grid container className={classes.heroSection}>
+            <Grid item xs={12} md={6}>
+                <Typography className={classes.heading}>
+                    {`Relax, Rejuvenate, Refresh`}
+                </Typography>
+                <Box className={classes.buttonContainer}>
+                    <Button variant='outlined' className={classes.button}>{`Schedule Appointment`}</Button>
+                    <Button variant='outlined' className={classes.button}>{`Book a Last Minute Appointment`}</Button>
+                    <Button variant='outlined' className={classes.button}>{`Purchase a Gift Card`}</Button>
+                </Box>
+            </Grid>
+            <Grid item xs={12} md={6}></Grid>
+        </Grid>
     )
 }
