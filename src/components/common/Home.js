7e4c117a9501620c66dc893b328f7eccfaa853c5
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2023-08-27 21:30:19 +0300

    #3: - add hero section (#16)
    * #3: - add hero section
    
    * #3 - change background image & add company copyright
    
    * #3 - change background image & add company copyright

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import { Card, CardContent, Button } from '@mui/material';
-import Carousel from 'react-material-ui-carousel';
-import { Link } from 'react-router-dom';
-import { carousel } from '../utils/carousel';
+import spa_one from "../assets/spa-2.jpg";
 import { makeStyles } from '@mui/styles';
+import { Button, Grid, Typography, Box } from '@mui/material';
+import Carousel from 'react-material-ui-carousel';
+import { carousel } from "../utils/carousel";
 
 const useStyles = makeStyles({
+    heroSection: {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${spa_one})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        padding: 6
+    },
+    heading: {
+        color: 'white',
+        fontSize: '3.5rem !important',
+        marginBottom: 2,
+        marginTop: "300px",
+        '@media (min-width:600px)': {
+            fontSize: '5rem'
+        },
+        whiteSpace: 'normal',
+        overflowWrap: 'break-word'
+    },
+    button: {
+        marginBottom: 2,
+        color: 'white !important',
+        borderColor: 'white',
+        borderRadius: 20,
+        '&:hover': {
+            backgroundColor: '#1976d2 !important'
+        }
+    },
+    buttonContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        width: '100%'
+    },
     carouselItem: {
         position: 'relative',
         overflow: 'hidden',
@@ -51,9 +88,21 @@ const useStyles = makeStyles({
 
 export const Home = () => {
     const classes = useStyles();
-
     return (
         <>
+            <Grid container className={classes.heroSection}>
+                <Grid item xs={12} md={6}>
+                    <Typography className={classes.heading}>
+                        {`Relax, Rejuvenate, Refresh`}
+                    </Typography>
+                    <Box className={classes.buttonContainer}>
+                        <Button variant='outlined' className={classes.button}>{`Schedule Appointment`}</Button>
+                        <Button variant='outlined' className={classes.button}>{`Book a Last Minute Appointment`}</Button>
+                        <Button variant='outlined' className={classes.button}>{`Purchase a Gift Card`}</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={6}></Grid>
+            </Grid>
             <Carousel>
                 {carousel.map((item, index) => (
                     <div key={index} className={classes.carouselItem}>
